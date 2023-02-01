8c1509b93eb0b80a19bbbd335bf36f073d39ece3
dayo4 <dayo4@live.com>
2023-02-01 08:09:14 +0100

    increased bodyparser limit

--- add-ons/index.js
+++ add-ons/index.js
@@ -1,11 +1,12 @@
 // Initialize App with primary plugins
 const express = require('express')
 const cors = require('cors')
+// const helmet = require("helmet")
 const bodyParser = require('body-parser')
-const app = express()
 
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.urlencoded({extended : true,limit:'20000000kb'}))
+app.use(bodyParser.json({limit:'20000000kb'}))
 app.use(cors(/* {
     origin:
     process.env.NODE_ENV === "development"
@@ -13,6 +14,8 @@ app.use(cors(/* {
     : "",
 } */))
 app.use(express.static('public'))
+// app.use(helmet())
+
 
 //Import Internal Plugins
 
